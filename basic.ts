ead0068f84723fddd46361a24f700a1b970b0752
bouzuya <m@bouzuya.net>
2025-05-29 11:45:20 +0900

    Add 4.x

--- basic.ts
+++ basic.ts
@@ -1,16 +1,16 @@
-import { parseBasic } from "npm:tiny-ts-parser";
+import { error, parseBasic } from "npm:tiny-ts-parser";
 
 type Term =
     | { tag: "true" }
     | { tag: "false" }
     | { tag: "if"; cond: Term; thn: Term; els: Term }
     | { tag: "number"; n: number }
-    | { tag: "add"; left: Term; right: Term; }
-    | { tag: "var"; name: string; }
-    | { tag: "func"; params: Param[]; body: Term; }
-    | { tag: "call"; func: Term; args: Term[]; }
-    | { tag: "seq"; body: Term; rest: Term; }
-    | { tag: "const"; name: string; init: Term; rest: Term; };
+    | { tag: "add"; left: Term; right: Term }
+    | { tag: "var"; name: string }
+    | { tag: "func"; params: Param[]; body: Term }
+    | { tag: "call"; func: Term; args: Term[] }
+    | { tag: "seq"; body: Term; rest: Term }
+    | { tag: "const"; name: string; init: Term; rest: Term };
 
 type Param = { name: string; type: Type };
 
@@ -29,11 +29,11 @@ function typecheck(t: Term, tyEnv: TypeEnv): Type {
             return { tag: "Boolean" };
         case "if": {
             const condTy = typecheck(t.cond, tyEnv);
-            if (condTy.tag !== "Boolean") throw "boolean expected";
+            if (condTy.tag !== "Boolean") error("boolean expected", t.cond);
             const thnTy = typecheck(t.thn, tyEnv);
             const elsTy = typecheck(t.els, tyEnv);
             if (!typeEq(thnTy, elsTy)) {
-                throw "then and else have different types";
+                error("then and else have different types", t);
             }
             return thnTy;
         }
@@ -41,13 +41,13 @@ function typecheck(t: Term, tyEnv: TypeEnv): Type {
             return { tag: "Number" };
         case "add": {
             const leftTy = typecheck(t.left, tyEnv);
-            if (leftTy.tag !== "Number") throw "number expected";
+            if (leftTy.tag !== "Number") error("number expected", t.left);
             const rightTy = typecheck(t.right, tyEnv);
-            if (rightTy.tag !== "Number") throw "number expected";
+            if (rightTy.tag !== "Number") error("number expected", t.right);
             return { tag: "Number" };
         }
         case "var": {
-            if (!(t.name in tyEnv)) throw `unknown variable: ${t.name}`;
+            if (!(t.name in tyEnv)) error(`unknown variable: ${t.name}`, t);
             return tyEnv[t.name];
         }
         case "func": {
@@ -60,15 +60,31 @@ function typecheck(t: Term, tyEnv: TypeEnv): Type {
         }
         case "call": {
             const funcTy = typecheck(t.func, tyEnv);
-            if (funcTy.tag !== "Func") throw "function expected";
-            if (t.args.length !== funcTy.params.length) throw "wrong number of arguments";
+            if (funcTy.tag !== "Func") error("function expected", t.func);
+            if (t.args.length !== funcTy.params.length) {
+                error("wrong number of arguments", t);
+            }
             for (let i = 0; i < t.args.length; i++) {
                 const argTy = typecheck(t.args[i], tyEnv);
                 if (!typeEq(argTy, funcTy.params[i].type)) {
-                    throw `parameter type mismatch: [${i}]: expected ${funcTy.params[i].type.tag}, got ${argTy.tag}`;
+                    error(
+                        `parameter type mismatch: [${i}]: expected ${
+                            funcTy.params[i].type.tag
+                        }, got ${argTy.tag}`,
+                        t.args[i],
+                    );
                 }
             }
             return funcTy.retType;
+        }
+        case "seq": {
+            typecheck(t.body, tyEnv);
+            return typecheck(t.rest, tyEnv);
+        }
+        case "const": {
+            const ty = typecheck(t.init, tyEnv);
+            const newTyEnv = { ...tyEnv, [t.name]: ty };
+            return typecheck(t.rest, newTyEnv);
         }
     }
 }
@@ -84,7 +100,9 @@ function typeEq(ty1: Type, ty2: Type): boolean {
             if (ty1.params.length !== ty2.params.length) return false;
             for (let i = 0; i < ty1.params.length; i++) {
                 // 仮引数の name の一致は確認しない
-                if (!typeEq(ty1.params[i].type, ty2.params[i].type)) return false;
+                if (!typeEq(ty1.params[i].type, ty2.params[i].type)) {
+                    return false;
+                }
             }
             return typeEq(ty1.retType, ty2.retType);
         }
@@ -114,7 +132,21 @@ function main() {
     // => { tag: "Boolean" }
 
     // console.log(typecheck(parseBasic("((x: boolean) => x)(42)"), {}));
-    // error: Uncaught (in promise) "parameter type mismatch: [0]: expected Boolean, got Number"
+    // => error: Uncaught (in promise) "parameter type mismatch: [0]: expected Boolean, got Number"
+
+    console.log(typecheck(
+        parseBasic(`
+        const add = (x: number, y: number) => x + y;
+        const select = (b: boolean, x: number, y: number) => b ? x : y;
+
+        const x = add(1, add(2, 3));
+        const y = select(true, x, x);
+
+        y;
+    `),
+        {},
+    ));
+    // => { tag: "Number" }
 
     // console.log(typecheck(parseBasic(`
     //     const add = (x: number, y: number) => x + y;
